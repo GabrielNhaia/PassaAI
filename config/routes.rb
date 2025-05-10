cc53a3a62660e10bf7a9b0e0e44cdf9066fdff57
Gabriel Nhaia <gabriel.nhaia@gmail.com>
2025-05-10 12:01:13 -0300

    remover rota de eventos de estudo e limpar código relacionado

--- config/routes.rb
+++ config/routes.rb
@@ -12,18 +12,14 @@ Rails.application.routes.draw do
   get 'edit_password', to: 'settings#edit_password'
   get 'edit_email', to: 'settings#edit_email'
   delete 'delete_account', to: 'settings#delete_account'
-  
-  get 'study_events', to: 'study_events#index'
+
   get 'progress_tracking', to: 'progress_tracking#index'
   
-  # Rota raiz dentro do devise_scope
   devise_scope :user do
     root to: 'devise/sessions#new'
   end
   
-  # Rotas protegidas que requerem autenticação
   authenticate :user do
-    # Adicione aqui outras rotas que precisam de autenticação
   end
 
   resources :study_events
