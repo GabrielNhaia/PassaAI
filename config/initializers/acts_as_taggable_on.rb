aa3578ad3045cb2d6e59e4d47a377186a4feef4e
Gabriel Nhaia <gabriel.nhaia@gmail.com>
2025-06-19 14:44:22 -0300

    refatorar inicialização do ActsAsTaggableOn para garantir configuração correta

--- config/initializers/acts_as_taggable_on.rb
+++ config/initializers/acts_as_taggable_on.rb
@@ -1,3 +1,5 @@
-ActsAsTaggableOn.force_binary_collation = true
-ActsAsTaggableOn.remove_unused_tags = true
-ActsAsTaggableOn.force_lowercase = true
+Rails.application.config.after_initialize do
+    ActsAsTaggableOn.force_binary_collation = true
+    ActsAsTaggableOn.remove_unused_tags = true
+    ActsAsTaggableOn.force_lowercase = true
+end
